2a244d50005d0a6d7b53154c180c2f2616e60e9f
Kageetai <ma@kageetai.net>
2024-01-31 12:23:57 +0100

    chore: script to generate notes in the vault for testing the plugin

---
+++ scripts/generateNotes.ts
@@ -0,0 +1,33 @@
+import { mkdirSync, readFileSync, writeFileSync } from "fs";
+import * as moment from "moment";
+
+interface PeriodicNotesSettings {
+	daily: {
+		format: string;
+		folder: string;
+		template: string;
+		enabled: boolean;
+	};
+}
+
+const vaultPath = `${process.cwd()}/${process.argv[2]}`;
+const years = process.argv[3];
+const today = moment();
+const yearsAgo = moment().subtract(years, "years");
+const settingsPath = `${vaultPath}/.obsidian/plugins/periodic-notes/data.json`;
+const settings = JSON.parse(
+	readFileSync(settingsPath).toString(),
+) as PeriodicNotesSettings;
+const dailyPath = `${vaultPath}${settings.daily.folder || "/"}`;
+
+for (let m = yearsAgo; m.diff(today, "days") <= 0; m.add(1, "days")) {
+	const dailyNoteFormat = m.format(settings.daily.format);
+	mkdirSync(
+		`${dailyPath}${dailyNoteFormat.split("/").slice(0, -1).join("/")}`,
+		{ recursive: true },
+	);
+	writeFileSync(
+		`${dailyPath}${dailyNoteFormat}.md`,
+		`daily Note for ${dailyNoteFormat}`,
+	);
+}
