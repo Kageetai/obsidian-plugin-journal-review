db7d1a0e3c835e5a7cace36ffe4d25549139464c
Michael Seifarth <Kageetai@users.noreply.github.com>
2024-02-20 15:57:25 +0100

    Remove preact compat (#24)
    * chore: remove all usages of preact-compat
    
    * chore: update dependencies

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -17,12 +17,6 @@ const context = await esbuild.context({
 	},
 	entryPoints: ["src/main.ts", "src/styles.css"],
 	bundle: true,
-	alias: {
-		react: "preact/compat",
-		"react-dom/test-utils": "preact/test-utils",
-		"react-dom": "preact/compat", // Must be below test-utils
-		"react/jsx-runtime": "preact/jsx-runtime",
-	},
 	external: [
 		"obsidian",
 		"electron",
