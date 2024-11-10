4bcf4e18ead9343ceb015a1f407566f6bc21c9d3
Kageetai <ma@kageetai.net>
2024-11-10 17:33:05 +0100

    chore: revert some changes regarding only using daily notes core plugins

--- scripts/generateNotes.ts
+++ scripts/generateNotes.ts
@@ -1,10 +1,13 @@
 import { mkdirSync, readFileSync, writeFileSync } from "fs";
 import * as moment from "moment";
 
-interface DailyNotesSettings {
-	format: string;
-	folder: string;
-	template: string;
+interface PeriodicNotesSettings {
+	daily: {
+		format: string;
+		folder: string;
+		template: string;
+		enabled: boolean;
+	};
 }
 
 const vaultPath = `${process.cwd()}/${process.argv[2]}`;
@@ -12,23 +15,22 @@ const years = process.argv[3];
 const notesPerDay = parseInt(process.argv[4]) || 1;
 const today = moment();
 const yearsAgo = moment().subtract(years, "years");
-const settingsPath = `${vaultPath}/.obsidian/daily-notes.json`;
+const settingsPath = `${vaultPath}/.obsidian/plugins/periodic-notes/data.json`;
 const settings = JSON.parse(
 	readFileSync(settingsPath).toString(),
-) as DailyNotesSettings;
-const dailyPath = `${vaultPath}${settings.folder || "/"}`;
+) as PeriodicNotesSettings;
+const dailyPath = `${vaultPath}${settings.daily.folder || "/"}`;
 
 for (let m = yearsAgo; m.diff(today, "days") <= 0; m.add(1, "days")) {
 	for (let i = 0; i < notesPerDay; i++) {
 		const innerM = m.clone().add(i, "hours");
-		const dailyNoteFormat = innerM.format(settings.format);
+		const dailyNoteFormat = innerM.format(settings.daily.format);
 
 		mkdirSync(
 			`${dailyPath}${dailyNoteFormat.split("/").slice(0, -1).join("/")}`,
 			{ recursive: true },
 		);
 
-		// console.log("innerM", innerM, innerM.format(settings.format));
 		writeFileSync(
 			`${dailyPath}${dailyNoteFormat}.md`,
 			`daily Note for ${dailyNoteFormat}`,
